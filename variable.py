238e4c26830ea94e5ef07797ce16de687ea1b62d
izeus1 <ihelios@gmail.com>
2018-12-10 16:11:15 +0900

    Initial commit

---
+++ variable.py
@@ -0,0 +1,17 @@
+
+# 변수 이름은 문자, 숫자, _로 구성된다.
+from builtins import print
+
+friend = 1
+my_name = "안대혁"
+myName = "안대혁"
+
+_yourname = "둘리"
+member1 = "도우넛"
+
+# print(keyword.kwlist)
+
+# 한글 이름 변수도 가능하다.
+가격1 = 2000
+print(가격1)
+
